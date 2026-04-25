6d57e4142ec5def0e50a196be28345f7d60a5216
Ashish Jayaswal <ashish4.jayaswal@gmail.com>
2026-04-25 22:26:17 +0530

    Update Employee.py

--- Class&Object/Instance_Variable/Employee.py
+++ Class&Object/Instance_Variable/Employee.py
@@ -1,11 +1,11 @@
 class Employee:
     def __init__(self):
-        self.employeeId = 101
-        self.employeeName = 'Ashish'
+        self.employeeId = 101 # Instance Variable
+        self.employeeName = 'Ashish' # Instance Variable
 
 e = Employee()
 print(e)
-print(e.employeeId, e.employeeName)
+print(e.employeeId, e.employeeName) # Instance Variable calling using object reference
 
 
 print(Employee.__dict__) # It print the dictionary of the Employee class
